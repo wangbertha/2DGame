503e09d5e9441a268d969160c295afc009fffcc9
wangbertha <wangbertha@gmail.com>
2024-12-08 20:59:33 -0600

    Refactor graphic and movement into entity package

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -7,11 +7,13 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import entity.Player;
+
 public class GamePanel extends JPanel implements Runnable {
     // Panel Settings
     final int originalTileSize = 16; // 16x16 pixels
     final int scale = 3;
-    final int tileSize = originalTileSize * scale;
+    public final int tileSize = originalTileSize * scale;
     final int maxScreenCol = 16; // 16 columns
     final int maxScreenRow = 12; // 12 rows
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
@@ -21,6 +23,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     // Initialize player position
     int playerX = 100;
@@ -63,15 +66,7 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-        if (keyH.upPressed == true) {
-            playerY -= playerSpeed;
-        } else if (keyH.downPressed == true) {
-            playerY += playerSpeed;
-        } else if (keyH.leftPressed == true) {
-            playerX -= playerSpeed;
-        } else if (keyH.rightPressed == true) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
@@ -79,8 +74,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         // Sample object
         Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
         g2.dispose();
     }
 }
