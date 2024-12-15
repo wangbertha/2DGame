a4afd56b44e4759ec7f51623b78cf5a4b24db073
wangbertha <wangbertha@gmail.com>
2024-12-14 21:39:56 -0600

    Draw and animate player

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,12 +1,16 @@
 package entity;
 
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
 public class Entity {
     public int worldX, worldY;
     public int speed;
 
     public String direction;
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public int imageNum = 1;
+    public int animCounter = 0;
 
     public Rectangle solidArea;
     public boolean collisionOn = false;
